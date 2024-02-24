5fb8a7c68d0a68a88cef6d2b3bafd8e8dce82ac2
Davysongs <Davidgsongs@gmail.com>
2024-02-24 14:58:31 +0100

    page urls

--- BankSite/urls.py
+++ BankSite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+# Wire up our API with the proper URL routing.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('base.urls')), # The home page is in base
+    path('auth/', include('accounts.urls')),
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def homepage(request):
+    return render(request, "index.html")
+# @login_required(login_url="login")
+def dashboard(request):
+    return render(request, "dashboard.html")
