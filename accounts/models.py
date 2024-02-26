7ac842a0eba84d1ee24d14805ec8c9810f3011cd
Davysongs <Davidgsongs@gmail.com>
2024-02-26 19:05:05 +0100

    account app

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model 
+
 
 # Create your models here.
 class Account(models.Model):
-    name = models.CharField(max_length=50)
+    user = models.ForeignKey(get_user_model(), on_delete = models.CASCADE)
     account_no = models.CharField(max_length=10, primary_key = True)
     balance = models.DecimalField(max_digits=8, decimal_places=2)
     regtime = models.DateTimeField(auto_now_add = True, help_text = "time of registeration")
+    image = models.ImageField(upload_to = 'Profile Pictures')
+    first_name = models.CharField(max_length = 100, blank = False)
+    last_name = models.CharField(max_length = 100, blank = False)
+    email = models.EmailField()
+    phone = models.CharField(blank = False, max_length = 11)
+    address = models.CharField(blank = False, max_length = 200)
+    city = models.CharField(blank = False, max_length = 200)
+    country = models.CharField(blank = False, max_length = 200)
+    postcode = models.CharField(blank = False, max_length = 200)
+    state = models.CharField(blank = False, max_length=200)
     def __str__(self):
         return self.name 
--- transactions/models.py
+++ transactions/models.py
@@ -10,7 +10,7 @@ class Trans(models.Model):
     name = models.CharField(max_length=120)
     bank = models.CharField(max_length=50)
     account_no = models.CharField(max_length=10)
-    amount = models.IntegerField()
+    amount =  models.DecimalField(max_digits=8, decimal_places=2)
     date = models.DateTimeField(auto_now_add = True, help_text = "time of transaction")
     transaction_ID = models.CharField(max_length=50, primary_key = True)
     info = models.CharField(max_length = 120)
