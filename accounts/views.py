bdbf435ad20dea6677d0da871837625bf7c4bf97
Davysongs <Davidgsongs@gmail.com>
2024-02-29 16:36:41 +0100

    login auth complete

--- accounts/views.py
+++ accounts/views.py
@@ -7,21 +7,18 @@ from login_required import login_not_required
 #login 
 @login_not_required
 def login_view(request):
-    if request.user.is_authenticated:
-        return redirect('dashboard')
-    else:
-        if request.method == "POST":
-            username = request.POST.get("username")
-            password = request.POST.get("password")
-            user = authenticate(request, username= username, password= password)
-            if user is not None:
-                login(request,user)
-                next_url = request.GET.get('next', 'dashboard')
-                return redirect(next_url)
-            else:
-                messages.info(request,"Username or password is incorrect")
-                return render(request, "login.html")
-        return render(request, "login.html")
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, email= email, password= password)
+        if user is not None:
+            login(request,user)
+            next_url = request.GET.get('next', 'dashboard')
+            return redirect(next_url)
+        else:
+            messages.info(request,"Username or password is incorrect")
+            return render(request, "login.html")
+    return render(request, "login.html")
 
 #sign up view
 @login_not_required
