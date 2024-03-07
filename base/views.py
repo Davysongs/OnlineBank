83a28caee1d00d8bd9dce6f58f93c939eed94b62
Davysongs <Davidgsongs@gmail.com>
2024-03-07 09:42:15 +0100

    profile logic

--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
 from accounts.models import Account
-from transactions.models import Trans
 from login_required import login_not_required
+from django.contrib.auth.hashers import make_password
+import random
+import string
+
 
 
 # Create your views here.
@@ -20,12 +23,22 @@ def dashboard(request):
             return redirect('profile')
     elif request.method=="POST":
         pass
+
+def generate_account_number():
+    while True:
+        # Generate a random string of numbers
+        account_no = ''.join(random.choices(string.digits, k=10))
+
+        # Check if the generated number already exists in the Account model
+        if not Account.objects.filter(account_no=account_no).exists():
+            # If the number doesn't exist, return it
+            return account_no
     
 def profile(request):
     if request.method == "GET":
         return render(request, "profile.html")
     if request.method == 'POST':
-        uid = request.user.email
+        uid=request.user.email
         print(uid)
         # Retrieve form data
         first_name = request.POST.get('first_name')
@@ -37,22 +50,25 @@ def profile(request):
         country = request.POST.get('country')
         postcode = request.POST.get('postcode')
         state = request.POST.get('state')
-        pin = request.POST.get('pin')
+        pin = request.POST.get('pin1')
+        print(pin, type(pin))
+        number =  generate_account_number() 
+        user = request.user
+            # If the user doesn't have an account, create a new one
+        account = Account.objects.create(
+            user=user,
+            pin=make_password(pin),    
+            account_no = number,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            phone=phone,
+            address=address,
+            city=city,
+            country=country,
+            postcode=postcode,
+            state=state,
+            balance=0,
+            )
+        return redirect('dashboard')
 
-        # Retrieve existing user profile instance
-        user_profile = Account.objects.get(email=uid)
-
-        # Update fields with new data
-        user_profile.first_name = first_name
-        user_profile.last_name = last_name
-        user_profile.email = email
-        user_profile.phone = phone
-        user_profile.address = address
-        user_profile.city = city
-        user_profile.country = country
-        user_profile.postcode = postcode
-        user_profile.state = state
-        user_profile.pin = pin
-
-        # Save the updated user profile instance
-        user_profile.save()
